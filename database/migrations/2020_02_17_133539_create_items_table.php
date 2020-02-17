18b44a22f0eecb67115f576f63970feda0afd24b
riskiwebid <riski.web.id@zoho.com>
2020-02-17 20:42:42 +0700

    Add model, migration and controller

---
+++ database/migrations/2020_02_17_133539_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id'); // yang menginput data
+            $table->string('name')->nullable(); // nama item
+            $table->string('price')->nullable(); // harga item
+            $table->text('image')->nullable(); // image
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
