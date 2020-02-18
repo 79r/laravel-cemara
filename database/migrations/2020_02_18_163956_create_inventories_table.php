7b91ee8652b87842da72002159ab84279d90dba8
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-19 00:40:10 +0700

    Create migration inventories and success migration with foreign key

---
+++ database/migrations/2020_02_18_163956_create_inventories_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('merk_id')->unsigned();
+            $table->bigInteger('supplier_id')->unsigned();
+
+            $table->unsignedDecimal('price', 12, 0)->nullable();
+            $table->unsignedBigInteger('qty')->nullable();
+
+            $table->text('image_url')->nullable();
+            $table->timestamp('years_of_purchase')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            
+            /** Foreign Key */
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('cascade');
+    
+            $table->foreign('category_id')->references('id')->on('categories')
+            ->onDelete('cascade');
+    
+            $table->foreign('merk_id')->references('id')->on('merks')
+            ->onDelete('cascade');
+    
+            $table->foreign('supplier_id')->references('id')->on('suppliers')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventorys');
+    }
+}
