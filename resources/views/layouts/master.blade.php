cc4149f23c2d7926c88beaf0dd8ea362a0d16e44
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-19 02:15:01 +0700

    Change footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -430,11 +430,11 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                2020 © Xoric.
+                                2020 © Laravel Cemara Inventory.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-right d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                                    Developed with <i class="mdi mdi-heart text-danger"></i> by <a href="https://riskiwebid.web.id" target="_blank">Riski</a>
                                 </div>
                             </div>
                         </div>
