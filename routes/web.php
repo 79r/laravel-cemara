8fa80ef66b42f684c18eec1c31b6f020826776b3
riskiwebid <riski.web.id@zoho.com>
2020-02-19 17:10:59 +0700

    Create route dashboard and update view

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
 Route::resource('inventory', 'InventoryController');
