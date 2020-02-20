e717e0b0aeaa8bf0c3903c2b0c5c70fce8874c80
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-20 10:38:02 +0700

    fix login and register view

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,32 +5,28 @@
         <meta charset="utf-8" />
         <title>Auth</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Register Cemara Inventory - Build with Laravel" name="description" />
+        <meta content="Auth Cemara Inventory - Build with Laravel" name="description" />
         <meta content="Riski" name="author" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('asset/images/favicon.ico') }}">
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
         <!-- Bootstrap Css -->
-        <link href="{{ asset('asset/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Icons Css -->
-        <link href="{{ asset('asset/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- App Css-->
-        <link href="{{ asset('asset/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
     </head>
 
     <body class="bg-primary bg-pattern">
-        <div class="home-btn d-none d-sm-block">
-            <a href="index.html"><i class="mdi mdi-home-variant h2 text-white"></i></a>
-        </div>
-
         <div class="account-pages my-5 pt-sm-5">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center mb-5">
-                            <a href="index.html" class="logo"><img src="{{ asset('asset/images/logo-light.png') }}" height="24" alt="logo"></a>
-                            <h5 class="font-size-16 text-white-50 mb-4">Responsive Bootstrap 4 Admin Dashboard</h5>
+                            <a href="" class="logo"><img src="{{ asset('images/logo.png') }}" height="24" alt="logo"></a>
+                            <h5 class="font-size-16 text-white-50 mb-4">Laravel Cemara Inventory</h5>
                         </div>
                     </div>
                 </div>
@@ -38,42 +34,7 @@
 
                 <div class="row justify-content-center">
                     <div class="col-xl-5 col-sm-8">
-                        <div class="card">
-                            <div class="card-body p-4">
-                                <div class="p-2">
-                                    <h5 class="mb-5 text-center">Register Account to Xoric.</h5>
-                                    <form class="form-horizontal" action="index.html">
-
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group form-group-custom mb-4">
-                                                    <input type="text" class="form-control" id="username" required>
-                                                    <label for="username">Username</label>
-                                                </div>
-                                                <div class="form-group form-group-custom mb-4">
-                                                    <input type="email" class="form-control" id="useremail" required>
-                                                    <label for="useremail">Email</label> 
-                                                </div>
-                                                <div class="form-group form-group-custom mb-4">
-                                                    <input type="password" class="form-control" id="userpassword" required>
-                                                    <label for="userpassword">Password</label>
-                                                </div>
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="term-conditionCheck">
-                                                    <label class="custom-control-label font-weight-normal" for="term-conditionCheck">I accept <a href="#" class="text-primary">Terms and Conditions</a></label>
-                                                </div>
-                                                <div class="mt-4">
-                                                    <button class="btn btn-success btn-block waves-effect waves-light" type="submit">Register</button>
-                                                </div>
-                                                <div class="mt-4 text-center">
-                                                    <a href="auth-login.html" class="text-muted"><i class="mdi mdi-account-circle mr-1"></i> Already have account?</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+                        @yield('content')
                     </div>
                 </div>
                 <!-- end row -->
@@ -82,11 +43,11 @@
         <!-- end Account pages -->
 
         <!-- JAVASCRIPT -->
-        <script src="{{ asset('asset/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('asset/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('asset/libs/metismenu/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('asset/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('asset/libs/node-waves/waves.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
 
         <script src="https://unicons.iconscout.com/release/v2.0.1/script/monochrome/bundle.js"></script>
 
