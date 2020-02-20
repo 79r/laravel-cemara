a983ca4f19c8c5278c6b76f9903a80d1aab6eac0
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-20 08:46:41 +0700

    rename migration table inventories to items

--- database/migrations/2020_02_18_163956_create_items_table.php
+++ database/migrations/2020_02_18_163956_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInventoriesTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateInventoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('inventories', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->bigInteger('user_id')->unsigned();
