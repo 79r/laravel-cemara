d05d6d3a1949d2ba78eae1719ee52182a5a4da93
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-20 10:43:07 +0700

    register database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        $this->call(UsersTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
+        $this->call(DivisionsTableSeeder::class);
+        $this->call(SuppliersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
