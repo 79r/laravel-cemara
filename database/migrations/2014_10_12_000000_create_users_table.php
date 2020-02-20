26418e19d9ab63e0424f71311dbe057897a14669
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-20 09:47:49 +0700

    fix migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration {
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('privilege')->default(1)->nullable()->comment('JSON Privilege');
+            $table->text('privilege')->default(null)->nullable()->comment('JSON Privilege');
             $table->tinyInteger('status')->default(1)->comment('0 is inactive');
             $table->timestamp('deleted_at')->default(null);
             $table->rememberToken();
