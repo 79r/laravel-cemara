3fa41167c037f7e49849008a38d8157e28ece3ee
riskiwebid <riski.web.id@zoho.com>
2020-02-21 15:34:05 +0700

    add footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-sm-12">
+                <div class="copyright">
+                    <p>2020 © Laravel Cemara Inventory.</p>
+                    <p> Developed with <i class="mdi mdi-heart text-danger"></i> by <a href="https://riskiwebid.web.id" target="_blank">Riski</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -64,19 +64,8 @@
                 </div>
                 <!-- End Page-content -->
 
+                @include('layouts.footer')
                 
-                <footer class="footer">
-                    <div class="container-fluid">
-                        <div class="row text-center">
-                            <div class="col-sm-12">
-                                <div class="copyright">
-                                    <p>2020 © Laravel Cemara Inventory.</p>
-                                    <p> Developed with <i class="mdi mdi-heart text-danger"></i> by <a href="https://riskiwebid.web.id" target="_blank">Riski</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
             </div>
             <!-- end main content-->
 
