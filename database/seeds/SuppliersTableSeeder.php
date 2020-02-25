fadc7bc05be8077b2c8ffebdf8060214c08e1808
riskiwebid <riski.web.id@zoho.com>
2020-02-25 18:16:49 +0700

    update seeder

--- database/seeds/SuppliersTableSeeder.php
+++ database/seeds/SuppliersTableSeeder.php
@@ -17,7 +17,7 @@ class SuppliersTableSeeder extends Seeder {
 
         $suppliers = array(
             array(
-                'name'      => 'Supplier Tidak Diketahui',
+                'name'      => 'Tidak Diketahui',
                 'phone'     => '0812 3456 7899',
                 'email'     => 'supplier1@example.com',
                 'address'   => 'Jl. Cemara No.67, Pasteur, Kec. Sukajadi, Kota Bandung, Jawa Barat 40161',
