79c37d2c0720372cdc581aef97678fd053a1485f
riskiwebid <riski.web.id@zoho.com>
2020-03-02 17:38:35 +0700

    update jo model

--- app/Jo.php
+++ app/Jo.php
@@ -3,7 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Jo extends Model {
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $table = "jo";
+    protected $fillable = array(
+        'serial_number',
+        'title',
+        'start_date',
+        'deadline',
+        'description',
+        'image_url',
+        'material',
+        'finishing',
+        'qty',
+        'notes',
+        'user_id',
+        'category_id',
+        'parent_id'
+    );
+
+    public function parent() {
+        return $this->belongsTo(JoParent::class);
+    }
+
 }
--- app/JoParent.php
+++ app/JoParent.php
@@ -5,5 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class JoParent extends Model {
-    //
+    public function jo() {
+        return $this->hasMany(Jo::class);
+    }
 }
