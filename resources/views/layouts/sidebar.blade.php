384bbdaaffb93bc153f71c230a7ea6d0c5b246bd
riskiwebid <riski.web.id@zoho.com>
2020-03-03 12:23:45 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
-                <li class="menu-title">Menu</li>
+                <li class="menu-title">Navigasi</li>
 
                 <li>
                     <a href="{{ route('dashboard') }}" class="waves-effect">
@@ -17,69 +17,62 @@
                 </li>
 
                 <li>
-                    <a href="{{ route('inventory.index') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-table"></i></div>
-                        <span>Inventory</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('inventory.create') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-comment-message"></i></div>
-                        <span>Input Data</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('inventory.import.excel') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-sign-in-alt"></i></div>
-                        <span>Import Excel</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('categories.index') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
-                        <span>Category</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('suppliers.index') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
-                        <span>Supplier</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('brands.index') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
-                        <span>Merek</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('divisions.index') }}" class="waves-effect">
-                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-box"></i></div>
-                        <span>Divisi</span>
-                    </a>
-                </li>
-
-                <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <div class="d-inline-block icons-sm mr-1"><i class="uim uim-layers-alt"></i></div>
-                        <span>Multi Level</span>
+                        <span>Inventory</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="javascript: void(0);">Level 1.1</a></li>
-                        <li><a href="javascript: void(0);" class="has-arrow">Level 1.2</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="javascript: void(0);">Level 2.1</a></li>
-                                <li><a href="javascript: void(0);">Level 2.2</a></li>
-                            </ul>
+                        <li>
+                            <a href="{{ route('inventory.index') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-table"></i></div>
+                                <span>Semua inventory</span>
+                            </a>
+                        </li>
+        
+                        <li>
+                            <a href="{{ route('inventory.create') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-comment-message"></i></div>
+                                <span>Input Data</span>
+                            </a>
+                        </li>
+        
+                        <li>
+                            <a href="{{ route('inventory.import.excel') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-sign-in-alt"></i></div>
+                                <span>Import Excel</span>
+                            </a>
+                        </li>
+        
+                        <li>
+                            <a href="{{ route('categories.index') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
+                                <span>Category</span>
+                            </a>
+                        </li>
+        
+                        <li>
+                            <a href="{{ route('suppliers.index') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
+                                <span>Supplier</span>
+                            </a>
+                        </li>
+        
+                        <li>
+                            <a href="{{ route('brands.index') }}" class="waves-effect">
+                                <div class="d-inline-block icons-sm mr-1"><i class="uim uim-document-layout-left"></i></div>
+                                <span>Merek</span>
+                            </a>
                         </li>
                     </ul>
                 </li>
+
+                <li>
+                    <a href="{{ route('jo.index') }}" class="waves-effect">
+                        <div class="d-inline-block icons-sm mr-1"><i class="uim uim-window-grid"></i></div>
+                        <span>Job List</span>
+                    </a>
+                </li>
+
 
             </ul>
 
