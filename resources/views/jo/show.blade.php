ff49a7ce00d5e7e7e7811f7eba9206a5705c70cd
riskiwebid <riski.web.id@zoho.com>
2020-03-04 17:26:04 +0700

    update single jo

--- resources/views/jo/show.blade.php
+++ resources/views/jo/show.blade.php
@@ -7,11 +7,11 @@
             <div class="card pl-sm-0 pl-md-4">
                 <div class="bg-transparent">
                     <div class="my-3">
-                        <a href="{{ route('jo.cemara.index') }}/{{ $jo->id }}/edit" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top" title="Edit">
+                        <a href="{{ route('jo.cemara.index') }}/{{ $jo->id }}/edit" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Edit">
                             <i class="mdi mdi-pencil"></i> <span>Edit JO ini</span>
                         </a>
                     </div>
-                    <h5 class="header-title-lg mb-3">{{ $jo->jo_code }}</h5>
+                    <h5 class="header-title-lg mb-3" data-toggle="tooltip" data-placement="top" title="{{ $jo->jo_code }}"> <span class="badge badge-primary">{{ $jo->jo_code }}</span> {{ $jo->title }}</h5>
                 </div>
                 <ul class="list-group list-group-flush">
 
@@ -20,15 +20,6 @@
                             <div class="media-body">
                                 <p class="text-muted mb-1">Location</p>
                                 <h5 class="mb-0">{{ $jo->parent->name }}</h5>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="list-group-item">
-                        <div class="media my-2">
-                            <div class="media-body">
-                                <p class="text-muted mb-1">Nama Jo</p>
-                                <h5 class="mb-0">{{ $jo->title }}</h5>
                             </div>
                         </div>
                     </li>
