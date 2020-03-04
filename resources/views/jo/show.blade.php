95335456e1f47862e020caceae30433d482143f8
riskiwebid <riski.web.id@zoho.com>
2020-03-04 17:46:25 +0700

    split jo single

--- resources/views/jo/show.blade.php
+++ resources/views/jo/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <div id="jo-item">
     <div class="row">
-        <div class="col-xl-7">
+
+        {{-- Left --}}
+        <div class="col-xl-6">
             <div class="card pl-sm-0 pl-md-4">
                 <div class="bg-transparent">
                     <div class="my-3">
@@ -14,44 +16,6 @@
                     <h5 class="header-title-lg mb-3" data-toggle="tooltip" data-placement="top" title="{{ $jo->jo_code }}"> <span class="badge badge-primary">{{ $jo->jo_code }}</span> {{ $jo->title }}</h5>
                 </div>
                 <ul class="list-group list-group-flush">
-
-                    <li class="list-group-item">
-                        <div class="media my-2">
-                            <div class="media-body">
-                                <p class="text-muted mb-1">Location</p>
-                                <h5 class="mb-0">{{ $jo->parent->name }}</h5>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="list-group-item">
-                        <div class="media my-2">
-                            <div class="media-body">
-                                <p class="text-muted mb-1">Klien</p>
-                                <h5 class="mb-0">{{ $jo->client->name }}</h5>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="list-group-item">
-                        <div class="media my-2">
-                            <div class="media-body">
-                                <p class="text-muted mb-1">Kontak Klien</p>
-                                <h5 class="mb-2">
-                                    <span><i class="mdi mdi-phone"></i></span>
-                                    <span>
-                                        {{ $jo->client->phone }}
-                                    </span>
-                                </h5>
-                                <h5 class="mb-0">
-                                    <span><i class="mdi mdi-email"></i></span>
-                                    <span>
-                                        {{ $jo->client->email }}
-                                    </span>
-                                </h5>
-                            </div>
-                        </div>
-                    </li>
 
                     <li class="list-group-item">
                         <div class="media my-2">
@@ -122,6 +86,55 @@
                                         <h6 class="mb-0">Nggak ada catatan untuk JO ini</h6>
                                     @endif
                                 </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        {{-- Right --}}
+        <div class="col-xl-6">
+            <div class="card pl-sm-0 pl-md-4">
+                <div class="bg-transparent">
+                    <div class="my-3">
+                        <a href="" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top" title="Show">
+                            <i class="mdi mdi-pencil"></i> <span>See more..</span>
+                        </a>
+                    </div>
+                    <h5 class="header-title-lg mb-3" data-toggle="tooltip" data-placement="top" title="{{ $jo->client->name }}">{[ Kontak Klien ]}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+
+                    <li class="list-group-item">
+                        <div class="media my-2">
+                            <div class="media-body">
+                                <p class="text-muted mb-1">Nama</p>
+                                <h5 class="mb-0">{{ $jo->client->name }}</h5>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="media my-2">
+                            <div class="media-body">
+                                <p class="text-muted mb-1">HP/Telp</p>
+                                <h5 class="mb-0">{{ $jo->client->phone }}</h5>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="media my-2">
+                            <div class="media-body">
+                                <p class="text-muted mb-1">Email</p>
+                                <h5 class="mb-0">{{ $jo->client->email }}</h5>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="media my-2">
+                            <div class="media-body">
+                                <p class="text-muted mb-1">Alamat</p>
+                                <h5 class="mb-0">{{ $jo->client->address }}</h5>
                             </div>
                         </div>
                     </li>
