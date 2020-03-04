156e3dac591fe7de5d07e5dd5c2f3c6eebfa34f7
riskiwebid <riski.web.id@zoho.com>
2020-03-04 14:54:10 +0700

    remove google font on header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,9 +23,6 @@
         <!-- Responsive datatable examples -->
         <link href="{{ asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css') }}" />  
 
-        <!-- google font -->
-        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i&display=swap" rel="stylesheet">
-
         <!-- Icons Css -->
         <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- App Css-->
