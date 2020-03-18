a64f1a03495d6e1cbb1dd24f8b58d7f0d781ff22
riskiwebid <riski.web.id@zoho.com>
2020-03-18 13:06:48 +0700

    add methods greeting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,26 @@ class AppServiceProvider extends ServiceProvider
 
         Blade::directive('currency', function ($expression) {
             return "Rp. <?php echo number_format($expression, 0, ',', '.'); ?>";
+        });
+
+        /** greeting */
+        Blade::directive('greeting', function () {
+            $hours      = (int)Carbon::parse(Carbon::now())->format('H');
+            $locale     = App::getLocale();
+            $message    = NULL;
+            if($hours >= 3 && $hours < 10) {
+                $message = 'Wilujeung Engjing';
+            }
+            else if($hours > 10 && $hours <= 15) {
+                $message = 'Good Siang';
+            }
+            else if($hours > 15 && $hours <= 18) {
+                $message = 'Good Sonten';
+            }
+            else {
+                $message = "Wilujeung Wengi";
+            }
+            return $message;
         });
     }
 }
